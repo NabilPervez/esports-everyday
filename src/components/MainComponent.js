984ccaa4fc99c1203378a0326b330bec30282f39
Nabil Pervez <perveznabil@gmail.com>
2020-10-26 19:16:41 -0500

    nav + foot

---
+++ src/components/MainComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() { 
+
+        return (
+            <div>
+                <Header />
+                
+                <Footer />
+            </div>
+        );
+    }
+}
+   
+export default Main;
