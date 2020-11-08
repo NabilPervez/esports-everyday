b671b7e5dd7d882f39713eaac392a404af4862cb
Nabil Pervez <perveznabil@gmail.com>
2020-11-08 17:02:00 -0600

    adding

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,24 +4,25 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Dataset from './DatasetComponent';
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
         };
     }
-
     render() { 
-
         return (
             <div>
                 <Header />
-                <Home />
-                <Dataset />
-                <About />
+                    <Switch>
+                        <Home />
+                        <Route path='/home' component = {Home} />
+                        <Route exact path='/dataset' render={() => <Dataset />} />
+                        <About exact path='/about' render={() => <About />}/>
+                        <Redirect to='/home' />
+                    </Switch>
                 <Footer />
             </div>
         );
