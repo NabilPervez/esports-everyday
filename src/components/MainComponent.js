5405086aba84de5d36befb23e83a4d93282676a5
Nabil Pervez <perveznabil@gmail.com>
2020-11-08 15:52:13 -0600

    about

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,7 +21,7 @@ class Main extends Component {
                 <Header />
                 <Home />
                 <Dataset />
-                <About/ >
+                <About />
                 <Footer />
             </div>
         );
